724d7d6503af93c5e3c599cf766d17195d925158
Timmy <timmy.1236@outlook.com>
2023-11-13 09:07:25 -0300

    Update consultar-faltas.sh

--- consultar-faltas.sh
+++ consultar-faltas.sh
@@ -4,13 +4,15 @@
 read -p "Ingrese el CI del docente que deseas consultar su faltas: " CI
 
 # Filtrar las líneas del archivo CSV para el docente específico
-faltas_docente=$(grep "^$CI:" "faltas.csv")
+faltas_docente=$(grep "$CI" "faltas.csv")
 
 if [ -n "$faltas_docente" ]; then
-    echo "Faltas del docente con CI $CI:"
-    echo "$faltas_docente" | while IFS=: read -r ci fecha tipo; do
-        echo "Falta registrada el $fecha: $tipo"
-    done
+
+    read -p "Ingrese el mes de las faltas que deseas buscar: " mes
+
+    echo "Las faltas del docente: $CI en el mes: $mes son..."
+
+    cat faltas.csv | grep $CI | grep $mes//*
 else
     echo "No se encontraron faltas para el docente con CI $CI"
 fi
