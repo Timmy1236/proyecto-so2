1f029c4d40cba19b110d91ff7ca3bd74092bef67
Timmy <timmy.1236@outlook.com>
2023-11-13 09:06:04 -0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -8,17 +8,16 @@ op=0
 
 # Todo lo que esta dentro de este while se va ejecutar repetidamente hasta que el usuario seleccione la opcion 5.
 while [ $op -ne 6 ]; do
-
 echo -e "-----------[MENU]-----------\n1) Registrar Docente.\n2) Registrar Inasistencia.\n3) Ingresar falta.\n4) Eliminar falta.\n5) Modificar falta.\n6) Salir."
+
 read -p "Seleccione una opcion: " op
 
 case $op in
-1) ./registrar-docente.sh;;
+1) ./registrar-docentes.sh;;
 2) ./registrar-inasistencia.sh;;
 3) ./altas.sh;;
 4) ./bajas.sh;;
-5) ./modificar;;
+5) ./modificar.sh;;
 6) echo "Saliendo del menu...";;
-*) echo "error";;
-esac
+*) echo "error";; esac
 done
